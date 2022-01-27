a70b6a23ed5e01a95f2364ce75f87396cfd071a9
Perla Aline Lorenzana Suárez <suarezperla.107@gmail.com>
2022-01-27 13:36:51 -0600

    Edit

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 var estudiantes = [
     {
-        apellidop: "Lorenzana",
-        apellidom: "Suarez",
-        nombre: "Perla Aline",
-        edad: 21,
-        fechanacimiento: {
-            año: 2000,
-            mes: "Septiembre",
-            dia: 14
-        },
-        carrera: "IRYC"
+      apellidop: "Lorenzana",
+      apellidom: "Suarez",
+      nombre: "Perla ALine",
+      edad: 21,
+      fechanacimiento: {
+        año: 2000,
+        mes: "septiembre",
+        dia: 14
+      },
+      carrera: "IRYC"
     },
     {
-        apellidop: "",
-        apellidom: "",
-        nombre: "",
-        edad: 1,
-        fechanacimiento: {
-            año: 1111,
-            mes: "",
-            dia: 1
-        },
-        carrera: "IRYC"
+      apellidop: "Cuellar",
+      apellidom: "Garces",
+      nombre: "Francisco",
+      edad: 22,
+      fechanacimiento: {
+  
+        año: 1999,
+        mes: "Septiembre",
+        dia: 15
+      },
+      carrera: "IRYC"
     }
-]
-
-var table = document.getElementByIdTagName("table")[0];
-for (var i = 0; i < estudiantes.length; i++) {
-var templ = document.getElementById('tbodyestudiantes').content.cloneNode(true); 
-var celnumero = templ.querySelector("span[name='tbodynumero']");
-var celnombre = templ.querySelector("span[name='tbodynombre']");
-var celedad = templ.querySelector("span[name='tbodyedad']");
-var celcarrera = templ.querySelector("span[name='tbodycarrera']");
-
-celnumero.innerText = i + 1
-celnombre.innerText = estudiantes[i].apellidop + + " " + estudiantes[i].apellidom + " " + estudiantes[i].nombre;
-celedad.innerText = estudiantes[i].edad;
-celcarrera.innerText = estudiantes[i].carrera
-
-table.append(templ);
-}
+  ]
+  
+  var table = document.getElementsByTagName("table")[0];
+  for (var i = 0; i < estudiantes.length; i++) {
+    var templ = document.getElementById('tbodyestudiantes').content.cloneNode(true);
+    var celnumero = templ.querySelector("span[name='tbodynumero']");
+    var celnombre = templ.querySelector("span[name='tbodynombre']");
+    var celedad = templ.querySelector("span[name='tbodyedad']");
+    var celcarrera = templ.querySelector("span[name='tbodycarrera']");
+  
+    celnumero.innerText = i + 1
+    celnombre.innerText = estudiantes[i].apellidop + " " + estudiantes[i].apellidom + " " + estudiantes[i].nombre;
+    celedad.innerText = estudiantes[i].edad;
+    celcarrera.innerText = estudiantes[i].carrera
+  
+    table.append(templ);
+  }
+  
